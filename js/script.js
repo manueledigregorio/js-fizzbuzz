0c4f2d41641f695402eab2e742275106145e38dc
manueledigregorio <dig.manuele@gmail.com>
2023-09-11 17:14:32 +0200

    [add] cicli di 3 e di 5

---
+++ js/script.js
@@ -0,0 +1,17 @@
+for ( let i=0; i<=100; i++){
+
+  if (i % 3 === 0){
+    console.log(i+ " è multiplo di 3")
+  }
+
+  if (i % 5 === 0) {
+    console.log(i+ " è multiplo di 5")
+  }
+
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log(i+ " è multiplo di 5 e di 3")
+  }
+
+
+
+}
